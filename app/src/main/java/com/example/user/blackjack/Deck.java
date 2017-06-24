e68540c7ef8d22380eb08bc13375a09c2870e64c
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-06-24 07:50:58 +0100

    Add Rank to Deck

--- app/src/main/java/com/example/user/blackjack/Deck.java
+++ app/src/main/java/com/example/user/blackjack/Deck.java
@@ -1,8 +1,28 @@
 package com.example.user.blackjack;
 
+import behaviours.Rank;
+import behaviours.Suit;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by user on 23/06/2017.
  */
 
 public class Deck {
+
+    public Rank deal(){
+        // Fill deck.
+        Rank[] ranks = Rank.values();
+
+        // return a random card from deck,
+        Random random = new Random();
+        int index = random.nextInt(ranks.length);
+
+        // include remove from deck once you combine
+        // Rank and Suit into Card
+
+        return ranks[index];
+    }
+
 }
